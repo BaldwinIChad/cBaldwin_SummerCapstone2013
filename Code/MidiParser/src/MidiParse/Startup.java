75452448e34ee609847ec01eb1523bede8a5249a
Chad Baldwin <wulfex@yahoo.com>
2013-07-10 16:29:02 -0600

    Parsed Midi File
    Went through each channel in each track using the MIDIMessages data and
    determine their musical note, whether the note is currently playing or
    not and display the velocity at which the note is played. Going to add
    the notes octave to the status and parse other MIDI files.

--- Code/MidiParser/src/MidiParse/Startup.java
+++ Code/MidiParser/src/MidiParse/Startup.java
@@ -4,27 +4,51 @@ import java.io.File;
 import java.io.IOException;
 
 import javax.sound.midi.InvalidMidiDataException;
+import javax.sound.midi.MetaMessage;
 import javax.sound.midi.MidiEvent;
+import javax.sound.midi.MidiMessage;
 import javax.sound.midi.MidiSystem;
 import javax.sound.midi.Sequence;
+import javax.sound.midi.ShortMessage;
 import javax.sound.midi.Track;
 
 public class Startup
 {
 	public static void main(String[] args)
 	{
-		int START = 0x90; //144 decimal
-		int STOP = 0x80; //128 decimal, what's in between
-		
+		final String[] notes = {"A", "A#", "B", "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#"};
 		Sequence seq;
 		try {
 			seq = MidiSystem.getSequence(new File("elise.mid"));
 			for(Track track : seq.getTracks())
 			{
+				int currentChannel = -1;
 				for(int eventIndex = 0; eventIndex < track.size(); eventIndex++)
 				{
 					MidiEvent event = track.get(eventIndex);
 					MidiMessage message = event.getMessage();
+					if(message instanceof ShortMessage)
+					{
+						ShortMessage a = (ShortMessage)message;
+						int channel = a.getChannel();
+						int velocity = a.getData2();
+						int pitch = (a.getData1() / 12);
+						String note = notes[pitch % 12];
+						String status = (a.getCommand() == ShortMessage.NOTE_ON) ? "On" : "Off";
+						
+						if(channel != currentChannel)
+						{
+							currentChannel = channel;
+							System.out.println("\n\nChannel:" + channel + "\n\tNote:" + note + "\\Status:" + status + "\\Velocity:" + velocity);
+						}
+						else
+							System.out.println("\tNote:" + note + " \\Status:" + status + " \\Velocity:" + velocity);
+					}
+					else if(message instanceof MetaMessage)
+					{
+						MetaMessage a = (MetaMessage)message;
+						System.out.println("METADATA--------- " + new String(a.getMessage()));
+					}
 				}
 			}
 		} catch (InvalidMidiDataException | IOException e) {
